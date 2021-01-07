5af92ec742ce8d5f92002d7585a0f0fc6c8ee23f
David Orson <onlyorson@outlook.com>
2021-01-07 20:17:43 +0000

    Reach Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Router, RouteComponentProps, Location } from "@reach/router";
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+const HomePg = (props: RouteComponentProps) => <Home />;
+const SignupPg = (props: RouteComponentProps) => <Signup />;
+const LoginPg = (props: RouteComponentProps) => <Login />;
 
 const App = () => {
   return (
-    <div>
-      hi
-    </div>
+    <Router>
+      <HomePg path="/" />
+      <SignupPg path="signup" />
+      <LoginPg path="login" />
+    </Router>
   )
 }
 
