1712c8f221996b31b765cc397dc52c2f372ef061
David Orson <onlyorson@outlook.com>
2021-01-07 20:18:14 +0000

    Login

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <p>
+        Home
+      </p>
+      <a href="/login">
+        Login
+      </a>
+      
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+/* import { loginUser } from "../redux/actions/userActions" */
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (email.includes('@')) {
+      console.log('email')
+    } else {
+      console.log('handle')
+    }
+  }
+
+  const handleChange = (e: any) => {
+    switch (e.target.name) {
+      case 'email':
+        setEmail(e.target.value)
+      return
+      case 'password':
+        setPassword(e.target.value)
+      return
+    }
+  }
+
+  return (
+    <div className="Login">
+      <h1>Login</h1>
+      <form className="form" onSubmit={handleSubmit} >
+        <label>Username or Email</label>
+        <input name="email" type="text" value={email} onChange={handleChange} />
+        <label>Password</label>
+        <input name="password" type="password" value={password} onChange={handleChange} />
+        <button>
+          submit
+        </button>
+      </form>
+      <a href="/">
+        Home
+      </a>
+      <a href="/signup">
+        Signup
+      </a>
+    </div>
+  )
+}
+
+export default Login;
