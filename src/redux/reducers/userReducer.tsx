b1fccad444f77ec4d96f0614e72f29544668b54a
David Orson <onlyorson@outlook.com>
2021-01-10 23:43:05 +0000

    setUser reducer set up2

--- src/redux/reducers/userReducer.tsx
+++ src/redux/reducers/userReducer.tsx
@@ -1,14 +1,14 @@
-import {
-  SET_USER
-} from '../types';
+import { SET_USER } from "../types";
 
 const userReducer = (state: any, action: any) => {
   switch (action.type) {
     case SET_USER:
       return {
         ...state,
-      }
+        authenticated: true,
+        ...action.payload,
+      };
   }
-}
+};
 
-export default userReducer 
+export default userReducer;
