cbe49a810649a30f2cf6583b9649ce4e2eefe2c8
David Orson <onlyorson@outlook.com>
2021-01-10 23:42:43 +0000

    setAction reducer set up

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,18 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from 'redux-thunk';
-
+import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+
+import userReducer from "./reducers/userReducer";
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+  user: userReducer,
+});
 
-const store = createStore(
-  reducers,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-)
+const store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
