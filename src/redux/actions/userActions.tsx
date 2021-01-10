11b5ccb5207802d0545102db433dad373e6a9747
David Orson <onlyorson@outlook.com>
2021-01-10 23:16:27 +0000

    setAuthorizationHeaders

--- src/redux/actions/userActions.tsx
+++ src/redux/actions/userActions.tsx
@@ -1,11 +1,15 @@
-import {
-  SET_USER
-} from "../types"
+import { SET_USER } from "../types";
+import axios from "axios";
 
+const setAuthorizationHeader = (token: any) => {
+  const FBIdToken = `Bearer ${token}`;
+  localStorage.setItem("FBIdToken", FBIdToken);
+  axios.defaults.headers.common["Authorization"] = FBIdToken;
+};
 
 export const loginUser = () => (dispatch: any) => {
   dispatch({
     type: SET_USER,
-    payload: 'test'
-  })
-}
+    payload: "test",
+  });
+};
