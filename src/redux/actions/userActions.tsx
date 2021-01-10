acb262a9742e299303bfdb8ecc84029b4adfde45
David Orson <onlyorson@outlook.com>
2021-01-10 23:31:05 +0000

    loginUser

--- src/redux/actions/userActions.tsx
+++ src/redux/actions/userActions.tsx
@@ -8,11 +8,14 @@ const setAuthorizationHeader = (token: any) => {
   axios.defaults.headers.common["Authorization"] = FBIdToken;
 };
 
-export const loginUser = () => (dispatch: any) => {
-  dispatch({
-    type: SET_USER,
-    payload: "test",
+export const loginUser = () => async (userData: any, dispatch: any) => {
+  const res = await axios.post("https://europe-west1-orson-home-app-3e05b.cloudfunctions.net/api/login", {
+    email: userData.email,
+    password: userData.password,
   });
+  setAuthorizationHeader(res.data.token);
+  dispatch(getUserData(dispatch));
+  navigate("/");
 };
 
 export const getUserData = async (dispatch: any) => {
