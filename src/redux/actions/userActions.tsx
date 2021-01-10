95f0fb05ac6d7e9f0c5db60621c9c249694bbaf8
David Orson <onlyorson@outlook.com>
2021-01-10 23:24:33 +0000

    signupUser

--- src/redux/actions/userActions.tsx
+++ src/redux/actions/userActions.tsx
@@ -1,5 +1,6 @@
 import { SET_USER } from "../types";
 import axios from "axios";
+import { navigate } from "@reach/router";
 
 const setAuthorizationHeader = (token: any) => {
   const FBIdToken = `Bearer ${token}`;
@@ -14,11 +15,24 @@ export const loginUser = () => (dispatch: any) => {
   });
 };
 
-const getUserData = async (dispatch: any) => {
+export const getUserData = async (dispatch: any) => {
   const userDetails = await axios.get("https://europe-west1-orson-home-app-3e05b.cloudfunctions.net/api/user");
 
   dispatch({
     type: SET_USER,
     payload: userDetails.data,
   });
+};
+
+export const signupUser = async (newUserData: any, dispatch: any) => {
+  const res: any = axios.post("https://europe-west1-orson-home-app-3e05b.cloudfunctions.net/api/signup", {
+    email: newUserData.email,
+    password: newUserData.password,
+    confirmPassword: newUserData.confirmPassword,
+    handle: newUserData.handle,
+  });
+  setAuthorizationHeader(res.data.token);
+
+  dispatch(getUserData(dispatch));
+  navigate("/");
 };
