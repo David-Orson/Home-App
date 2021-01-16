900b399a804a24e6bb30342aefbbe64b214fb702
David Orson <onlyorson@outlook.com>
2021-01-16 20:44:53 +0000

    Adding signup users to db

--- functions/functions/handlers/users.js
+++ functions/functions/handlers/users.js
@@ -1,3 +1,5 @@
+const { admin, db } = require("../utility/admin");
+
 const firebaseConfig = require("../utility/firebaseConfig");
 
 const firebase = require("firebase");
@@ -6,29 +8,43 @@ firebase.initializeApp(firebaseConfig);
 exports.signup = async (req, res) => {
   try {
     const newUser = {
-    handle: req.body.handle,
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-  }
+      handle: req.body.handle,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+    };
 
-  const data = await firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    let token, userId;
 
-  let token, userId;
+    const doc = await db.doc(`/users/${newUser.handle}`).get();
 
-  userId = data.user.uid
+    const data = doc.exists
+      ? res.status(400).json({ handle: "This username is unavailable" })
+      : await firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
 
-  const idToken = await data.user.getIdToken();
+    userId = data.user.uid;
 
-  token = idToken;
+    const idToken = await data.user.getIdToken();
 
-  return res.status(201).json({ /* user: `user ${data.user.uid} signed up successfully` */ token })
-  }
-  catch (err) {
+    token = idToken;
+
+    const userCredentials = {
+      handle: newUser.handle,
+      email: newUser.email,
+      createdAt: new Date().toISOString(),
+      userId,
+    };
+
+    const done = await db.doc(`/users/${newUser.handle}`).set(userCredentials);
+
+    if (done) {
+      return res.status(201).json({ token });
+    }
+  } catch (err) {
     console.error(err);
-    return res.status(500).json ({ error: err.code })
-  }    
-}
+    return res.status(500).json({ error: err.code });
+  }
+};
 
 exports.login = async (req, res) => {
   try {
@@ -42,9 +58,8 @@ exports.login = async (req, res) => {
     const token = await data.user.getIdToken();
 
     return res.status(200).json({ token });
-  }
-  catch (err) {
+  } catch (err) {
     console.err(err);
-    return res.status(403).json({ general: "Wrong credentials, please try again" })
+    return res.status(403).json({ general: "Wrong credentials, please try again" });
   }
-}
+};
--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,35 +1,11 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
 const express = require("express");
 const app = express();
-/* const admin = require("firebase-admin"); */
-/* const app = require("express")();
-admin.initializeApp(); */
 
 const cors = require("cors");
 app.use(cors());
 
 const { signup, login } = require("./handlers/users");
-
-/* const firebase = require("firebase");
-firebase.initializeApp(config); */
-
-/* app.get('/HelloWorld', (req, res) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  res.send("Hello from Firebase!");
-});
-
-app.post('./signup', (req, res) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle,
-  };
-
-  firebase.auth().createUser
-}) */
 
 app.post("/signup", signup);
 app.post("/login", login);
